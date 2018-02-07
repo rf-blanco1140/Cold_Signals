5572eef2aeac399644cb771121fd039b0ff9d1b2
Juliana Montes <j.montes495@uniandes.edu.co>
2018-02-07 10:47:28 -0500

    nueva antena

---
+++ Assets/Scripts/RadioSignal.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioSignal : MonoBehaviour {
+
+    public AudioClip audio;
+
+    bool inArea;
+
+    public AudioSource audioSource;
+
+    float diametroTransmision = 0;
+
+    GameObject jugador;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (inArea)
+        {
+            float distancia = Mathf.Abs((jugador.transform.position - this.transform.position).magnitude);
+            float volumen = diametroTransmision / (distancia*10);
+            audioSource.volume = volumen;
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            inArea = true;
+            jugador = other.gameObject;
+            if (diametroTransmision == 0)
+            {
+                diametroTransmision = Mathf.Abs((jugador.transform.position - this.transform.position).magnitude);
+            }
+            audioSource.PlayOneShot(audio);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            inArea = false;
+            jugador = null;
+            audioSource.Stop();
+        }
+    }
+    
+}
