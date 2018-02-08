5d9516414e75e73923199514d23e5f006481256a
Raul Felipe Blanco <ocdshiro@gmail.com>
2018-02-08 13:35:45 -0500

    Organizacion de archivos y prefabs
    Prefabs para de mesas creados, tambien se organizo scripts

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    //-------------------------------------------------------------------
+    // Variables
+    //-------------------------------------------------------------------
+
+    public GameObject radio;
+
+    public GameObject barras;
+
+
+    //-------------------------------------------------------------------
+    // Metodos
+    //-------------------------------------------------------------------
+
+    // Use this for initialization
+    void Start () {
+        radio.SetActive(false);
+        barras.SetActive(true);
+    }
+    
+    /// <summary>
+    /// Activa y desactiva la radio en base a su estado actual
+    /// tambien deactiva las barra de calor y hambre
+    /// </summary>
+    public void toggleRadio()
+    {
+        radio.SetActive(!radio.activeInHierarchy);
+        barras.SetActive(!barras.activeInHierarchy);
+    }
+}
