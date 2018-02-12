1cc59035aa09ed4557994be491004ba220eea8a4
Raul Felipe Blanco <ocdshiro@gmail.com>
2018-02-12 18:27:49 -0500

    Progreso puzzle 1
    Creacion e implementacion de script que selecciona color de arbol al azar
    Adicion de items recolectables y de historia en el mapa

---
+++ Assets/Scripts/Forest/TreeSprite.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSprite : MonoBehaviour
+{
+
+    //-------------------------------------------------------------------
+    // Variables
+    //-------------------------------------------------------------------
+
+    // Lista que contiene los sprites posibles que puede tener un arbol
+    public Sprite[] treeSpriteOptions;
+
+
+
+    //-------------------------------------------------------------------
+    // Metodos
+    //-------------------------------------------------------------------
+
+    // Use this for initialization
+    void Start()
+    {
+        int treeId = Random.Range(0,2);
+        Sprite treeSprite = treeSpriteOptions[treeId];
+        this.GetComponent<SpriteRenderer>().sprite = treeSprite;
+    }
+}
