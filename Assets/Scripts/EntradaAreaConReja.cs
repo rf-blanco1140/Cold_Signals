99dc866a60656cc67e96ad843eab0b004e8951da
Raul Felipe Blanco <ocdshiro@gmail.com>
2018-02-26 10:40:17 -0500

    Arreglos al comportamiento de reja en P3
    Se hizo que la reja se renderice por delante o por detras del jugador dependiendo de donde este este parado

---
+++ Assets/Scripts/EntradaAreaConReja.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntradaAreaConReja : MonoBehaviour
+{
+    //-------------------------------------------------------------------
+    // Variables
+    //-------------------------------------------------------------------
+
+    public bool estaAdentro;
+
+    public GameObject parejaPuerta;
+
+    //-------------------------------------------------------------------
+    // Metodos
+    //-------------------------------------------------------------------
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            jugadorEntroSalio();
+            Debug.Log("esta adentro: " + estaAdentro);
+        }
+    }
+
+    public void jugadorEntroSalio()
+    {
+        if(estaAdentro == false)
+        {
+            estaAdentro = true;
+        }
+        else
+        {
+            estaAdentro = false;
+        }
+    }
+	
+}
