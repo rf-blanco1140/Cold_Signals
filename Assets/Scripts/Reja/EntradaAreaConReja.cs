d3f7fc3a73cdd7b01e48ed34f26dbe73603fbda9
Raul Felipe Blanco <ocdshiro@gmail.com>
2018-02-26 15:39:34 -0500

    Renderizacion de reja en base a posicion de jugador
    Se implemento la funcionalidad que renderiza la reja encima o debajo del jugador dependiendo en donde se encuentre este

--- Assets/Scripts/Reja/EntradaAreaConReja.cs
+++ Assets/Scripts/Reja/EntradaAreaConReja.cs
@@ -10,18 +10,49 @@ public class EntradaAreaConReja : MonoBehaviour
 
     public bool estaAdentro;
 
+    public bool esDentroDeLaReja;
+
+    public GameObject pairEntrance;
+
+    public GameObject thisRejaColliders;
+
+    private GameObject ladoDer;
+
+    private GameObject ladoIzq;
+
+    private GameObject ladoSup;
+
+    private GameObject ladoInfIzq;
+
+    private GameObject ladoInfDer;
 
 
     //-------------------------------------------------------------------
     // Metodos
     //-------------------------------------------------------------------
 
+    private void Start()
+    {
+        ladoDer = GameObject.Find("ladoDerecho");
+        ladoIzq = GameObject.Find("ladoIzquierdo");
+        ladoSup = GameObject.Find("LadoSuperior");
+        ladoInfIzq = GameObject.Find("BajaIzq");
+        ladoInfDer = GameObject.Find("BajaDer");
+
+        layerFueraDeReja();
+
+        if(esDentroDeLaReja)
+        {
+            thisRejaColliders.SetActive(false);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
+            cambioDePuerta();
             jugadorEntroSalio();
-            Debug.Log("esta adentro: " + estaAdentro);
         }
     }
 
@@ -30,11 +61,75 @@ public class EntradaAreaConReja : MonoBehaviour
         if(estaAdentro == false)
         {
             estaAdentro = true;
+            layerDentroReja();
+            if(esDentroDeLaReja)
+            {
+                thisRejaColliders.SetActive(true);
+                pairEntrance.GetComponent<EntradaAreaConReja>().thisRejaColliders.SetActive(false);
+            }
         }
         else
         {
             estaAdentro = false;
+            layerFueraDeReja();
+            if (!esDentroDeLaReja)
+            {
+                thisRejaColliders.SetActive(true);
+                pairEntrance.GetComponent<EntradaAreaConReja>().thisRejaColliders.SetActive(false);
+            }
         }
     }
-	
+
+    /// <summary>
+    /// Activa el collider de la puerta contraria a esta
+    /// Se considera una puerta contraria la puerta de salida si esta es la de entrada o viceversa
+    /// </summary>
+    public void cambioDePuerta()
+    {
+        pairEntrance.SetActive(true);
+        this.transform.gameObject.SetActive(false);
+    }
+
+    public void layerFueraDeReja()
+    {
+        for(int i =0; i<ladoInfIzq.transform.childCount; i++)
+        {
+            GameObject esteHijo = ladoInfIzq.transform.GetChild(i).gameObject;
+            esteHijo.GetComponent<SpriteRenderer>().sortingOrder = 2;
+        }
+
+        for (int i = 0; i < ladoInfDer.transform.childCount; i++)
+        {
+            GameObject esteHijo = ladoInfDer.transform.GetChild(i).gameObject;
+            esteHijo.GetComponent<SpriteRenderer>().sortingOrder = 2;
+        }
+
+        for (int i = 0; i < ladoSup.transform.childCount; i++)
+        {
+            GameObject esteHijo = ladoSup.transform.GetChild(i).gameObject;
+            esteHijo.GetComponent<SpriteRenderer>().sortingOrder = 4;
+        }
+    }
+
+    public void layerDentroReja()
+    {
+        for (int i = 0; i < ladoInfIzq.transform.childCount; i++)
+        {
+            GameObject esteHijo = ladoInfIzq.transform.GetChild(i).gameObject;
+            esteHijo.GetComponent<SpriteRenderer>().sortingOrder = 4;
+        }
+
+        for (int i = 0; i < ladoInfDer.transform.childCount; i++)
+        {
+            GameObject esteHijo = ladoInfDer.transform.GetChild(i).gameObject;
+            esteHijo.GetComponent<SpriteRenderer>().sortingOrder = 4;
+        }
+
+        for (int i = 0; i < ladoSup.transform.childCount; i++)
+        {
+            GameObject esteHijo = ladoSup.transform.GetChild(i).gameObject;
+            esteHijo.GetComponent<SpriteRenderer>().sortingOrder = 2;
+        }
+    }
+
 }
