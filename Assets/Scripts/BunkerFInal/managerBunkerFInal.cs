560b3cc7d5f8f4646e2f5c40a6ed16c7bb00e3b7
Raul Felipe Blanco <ocdshiro@gmail.com>
2018-03-19 12:55:59 -0500

    Primera version condiciones de victoria del juego
    aun hace falta pulirlo y falta la condicion para las rejas

---
+++ Assets/Scripts/BunkerFInal/managerBunkerFInal.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class managerBunkerFInal : MonoBehaviour
+{
+
+    public GameObject puertaCongelada;
+
+    public GameObject puertaCerrada;
+
+    public GameObject lectorActivado;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    // Si la puerta esta congelada el Lector Activado tiene el collider desactivado
+    // Si se activa primero el Lector que descongelar la puerte --> Collider desactivado
+    // De lo contrario se activa el lector y su collider
+
+
+    public void descongelarPuerta()
+    {
+        lectorActivado.GetComponent<BoxCollider2D>().enabled = true;
+        puertaCerrada.SetActive(true);
+        puertaCongelada.SetActive(false);
+    }
+}
